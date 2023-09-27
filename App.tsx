8017ec2a1ae473fd7cc2b18a9d92c16bd300154a
Sergio Navarrete <sergio.nava89@gmail.com>
2023-09-27 22:56:09 +0100

    Navigation, profile page

--- App.tsx
+++ App.tsx
@@ -1,144 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, View, Text } from "react-native";
-import AListItem from "./component/AListItem";
-import AddItemModal from "./component/AddItemModal";
-import ConfirmationModal from "./component/ConfirmationModal";
-import {
-  getAllItems,
-  saveItem,
-  removeItem as storageRemoveItem,
-} from "./component/Storage";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import React from "react";
+import Main from "./component/Main";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Settings from "./component/Profile";
+import { Button } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [alistItems, setAListItems] = useState([] as AListItem[]);
-  const [selectedItem, setSelectedItem] = useState(null as AListItem | null);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [confirmationModalVisible, setConfirmationModalVisible] =
-    useState(false);
-
-  const loadItemsFromLocalStorage = async () => {
-    try {
-      getAllItems().then((items) => {
-        setAListItems(items);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const removeItem = async (item: AListItem | null) => {
-    if (item !== null) {
-      await storageRemoveItem(item);
-      loadItemsFromLocalStorage();
-    }
-  };
-
-  const hideModal = () => {
-    setModalVisible(false);
-    setSelectedItem(null);
-  };
-  const showModal = () => setModalVisible(true);
-
-  useEffect(() => {
-    loadItemsFromLocalStorage();
-  }, []);
-
   return (
-    <View style={styles.container}>
-      {alistItems.length > 0 ? (
-        <FlatList
-          style={{ alignSelf: "stretch", flex: 0.8, marginBottom: 20 }}
-          data={alistItems}
-          renderItem={({ item }) => (
-            <AListItem
-              item={item}
-              removeItem={(item: AListItem) => {
-                setSelectedItem(item);
-                setConfirmationModalVisible(true);
-              }}
-              editItem={(item: AListItem) => {
-                setSelectedItem(item);
-                setModalVisible(true);
-              }}
-            ></AListItem>
-          )}
-          keyExtractor={(item, index) => item.name}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Main}
+          options={({ navigation }) => ({
+            title: "Home",
+            headerStyle: {
+              backgroundColor: "#273469",
+            },
+            headerTintColor: "#EBF2FA",
+            headerRight: () => (
+              <Button
+                onPress={() => navigation.navigate("Profile")}
+                title="Profile"
+              />
+            ),
+          })}
         />
-      ) : (
-        <View style={{ alignContent: "center" }}>
-          <Text style={{ fontSize: 22, textAlign: "center" }}>
-            Tap{" "}
-            <Ionicons
-              name="add-circle"
-              style={(styles.fab, { marginLeft: 10 })}
-              size={25}
-              onPress={() => setModalVisible(true)}
-            />{" "}
-            to add a new item
-          </Text>
-        </View>
-      )}
-
-      {modalVisible ? (
-        <AddItemModal
-          item={selectedItem}
-          saveItem={async (item: AListItem) => {
-            await saveItem(item);
-            await loadItemsFromLocalStorage();
+        <Stack.Screen
+          name="Profile"
+          component={Settings}
+          options={{
+            title: "Profile",
+            headerStyle: {
+              backgroundColor: "#273469",
+            },
+            headerTintColor: "#EBF2FA",
           }}
-          hideModal={hideModal}
-          showModal={showModal}
-          visible={modalVisible}
         />
-      ) : (
-        <Ionicons
-          name="add-circle"
-          style={styles.fab}
-          size={60}
-          onPress={() => setModalVisible(true)}
-        />
-      )}
-      <ConfirmationModal
-        message={"Are you sure you wish to delete " + selectedItem?.name}
-        visible={confirmationModalVisible}
-        item={selectedItem}
-        acceptCallbackFn={() => {
-          removeItem(selectedItem);
-          setSelectedItem(null);
-          setConfirmationModalVisible(false);
-        }}
-        rejectCallbackFn={() => {
-          setSelectedItem(null);
-          setConfirmationModalVisible(false);
-        }}
-        hideModalFn={() => {
-          setSelectedItem(null);
-          setConfirmationModalVisible(false);
-        }}
-      ></ConfirmationModal>
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-  fab: {
-    width: 60,
-    height: 60,
-    position: "absolute",
-    bottom: 20,
-    right: 20,
-    borderRadius: 100,
-    alignContent: "center",
-  },
-});
---
+++ component/Main.tsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import AListItem from "./AListItem";
+import AddItemModal from "./AddItemModal";
+import ConfirmationModal from "./ConfirmationModal";
+import {
+  getAllItems,
+  saveItem,
+  removeItem as storageRemoveItem,
+} from "./Storage";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function Main({ navigation: any }) {
+  const [alistItems, setAListItems] = useState([] as AListItem[]);
+  const [selectedItem, setSelectedItem] = useState(null as AListItem | null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [confirmationModalVisible, setConfirmationModalVisible] =
+    useState(false);
+
+  const loadItemsFromLocalStorage = async () => {
+    try {
+      getAllItems().then((items) => {
+        setAListItems(items);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const removeItem = async (item: AListItem | null) => {
+    if (item !== null) {
+      await storageRemoveItem(item);
+      loadItemsFromLocalStorage();
+    }
+  };
+
+  const hideModal = () => {
+    setModalVisible(false);
+    setSelectedItem(null);
+  };
+  const showModal = () => setModalVisible(true);
+
+  useEffect(() => {
+    loadItemsFromLocalStorage();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {alistItems.length > 0 ? (
+        <FlatList
+          style={{ alignSelf: "stretch", flex: 0.8, marginBottom: 20 }}
+          data={alistItems}
+          renderItem={({ item }) => (
+            <AListItem
+              item={item}
+              removeItem={(item: AListItem) => {
+                setSelectedItem(item);
+                setConfirmationModalVisible(true);
+              }}
+              editItem={(item: AListItem) => {
+                setSelectedItem(item);
+                setModalVisible(true);
+              }}
+            ></AListItem>
+          )}
+          keyExtractor={(item, index) => item.name}
+        />
+      ) : (
+        <View style={{ alignContent: "center" }}>
+          <Text style={{ fontSize: 22, textAlign: "center" }}>
+            Tap{" "}
+            <Ionicons
+              name="add-circle"
+              style={(styles.fab, { marginLeft: 10 })}
+              size={25}
+              onPress={() => setModalVisible(true)}
+            />{" "}
+            to add a new item
+          </Text>
+        </View>
+      )}
+
+      {modalVisible ? (
+        <AddItemModal
+          item={selectedItem}
+          saveItem={async (item: AListItem) => {
+            await saveItem(item);
+            await loadItemsFromLocalStorage();
+          }}
+          hideModal={hideModal}
+          showModal={showModal}
+          visible={modalVisible}
+        />
+      ) : (
+        <Ionicons
+          name="add-circle"
+          style={styles.fab}
+          size={60}
+          onPress={() => setModalVisible(true)}
+        />
+      )}
+      <ConfirmationModal
+        message={"Are you sure you wish to delete " + selectedItem?.name}
+        visible={confirmationModalVisible}
+        item={selectedItem}
+        acceptCallbackFn={() => {
+          removeItem(selectedItem);
+          setSelectedItem(null);
+          setConfirmationModalVisible(false);
+        }}
+        rejectCallbackFn={() => {
+          setSelectedItem(null);
+          setConfirmationModalVisible(false);
+        }}
+        hideModalFn={() => {
+          setSelectedItem(null);
+          setConfirmationModalVisible(false);
+        }}
+      ></ConfirmationModal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  fab: {
+    width: 60,
+    height: 60,
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    borderRadius: 100,
+    alignContent: "center",
+  },
+});
