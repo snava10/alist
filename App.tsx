aaf7a9a07de79abb63f026553a6bbb6c12530960
Sergio Navarrete <sergio.nava89@gmail.com>
2023-10-07 15:11:51 +0100

    aligning center

--- App.tsx
+++ App.tsx
@@ -108,7 +108,14 @@ export default function App() {
           />
         </View>
       ) : (
-        <View style={{ alignContent: "center" }}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <Text style={{ fontSize: 22, textAlign: "center" }}>
             Tap{" "}
             <Ionicons
