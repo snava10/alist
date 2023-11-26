c472522d9cdc30e0f8bd85961c839e164fb45493
Sergio Navarrete <sergio.nava89@gmail.com>
2023-11-26 13:25:52 +0000

    succesfully aunthenticating

--- component/Login/GoogleLogin.tsx
+++ component/Login/GoogleLogin.tsx
@@ -8,22 +8,32 @@ import auth from "@react-native-firebase/auth";
 
 GoogleSignin.configure({
   webClientId:
-    "183439267259-kttd199p50pegq0maqdv4kv6v0nau62d.apps.googleusercontent.com",
+    // "183439267259-3fdekennuneoalsccieeaj44v781r3b4.apps.googleusercontent.com",
+    "183439267259-12mtdreenqc4shhl3ab9ddnbhb00auu0.apps.googleusercontent.com",
+  // "183439267259-4h0561svmr0ab771gq0nlgpj4640g459.apps.googleusercontent.com",
+  // "183439267259-lte4rjvdj437i3r8904ck1vsflf7k7dv.apps.googleusercontent.com",
 });
 
 async function onGoogleButtonPress() {
-  // Check if your device supports Google Play
-  await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-  // Get the users ID token
-  const { idToken } = await GoogleSignin.signIn();
-  console.log(idToken);
+  try {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+    console.log(idToken);
 
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-  console.log(googleCredential);
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    console.log(googleCredential);
 
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  } catch (e: any) {
+    console.log("Error signing in with google" + e);
+    console.log(e.code);
+    console.log(e.message);
+    throw e;
+  }
 }
 
 export default function GoogleLogin() {
