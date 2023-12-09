5362fb1e39d97076814986137dcd42c03dde7f30
Sergio Navarrete <sergio.nava89@gmail.com>
2023-12-09 19:53:15 +0000

    Authentication component

--- component/Login/AuthenticationComponent.tsx
+++ component/Login/AuthenticationComponent.tsx
@@ -35,13 +35,13 @@ export default function AuthenticationComponent({
   });
 
   return _isLoggedIn ? (
-    <View style={styles.container}>
+    <View>
       <View style={styles.child_view}>
         <Button title="Log Out" onPress={logOutFn} />
       </View>
     </View>
   ) : (
-    <View style={styles.container}>
+    <View>
       <View style={styles.child_view}>
         {authProviders.facebook ? <FacebookLogin></FacebookLogin> : <></>}
         {authProviders.google ? (
@@ -50,19 +50,48 @@ export default function AuthenticationComponent({
           <></>
         )}
         {authProviders.allowAnonymous ? (
-          <Pressable
-            style={[{}, globalStyles.button.primary.main]}
-            onPress={() => {
-              setIsLoggedIn(true);
-              if (continueAnonymousCallbackFn) {
-                continueAnonymousCallbackFn();
-              }
-            }}
-          >
-            <Text style={globalStyles.button.text.defaut}>
-              Continue anonymously
-            </Text>
-          </Pressable>
+          <>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                paddingVertical: 10,
+              }}
+            >
+              <View
+                style={{ flex: 1, height: 1, backgroundColor: "#b0afac" }}
+              />
+              <View>
+                <Text
+                  style={{
+                    width: 50,
+                    textAlign: "center",
+                    color: "#b0afac",
+                    fontWeight: "bold",
+                    fontSize: 20,
+                  }}
+                >
+                  or
+                </Text>
+              </View>
+              <View
+                style={{ flex: 1, height: 1, backgroundColor: "#b0afac" }}
+              />
+            </View>
+            <Pressable
+              style={[globalStyles.button, globalStyles.button.primary.main]}
+              onPress={() => {
+                setIsLoggedIn(true);
+                if (continueAnonymousCallbackFn) {
+                  continueAnonymousCallbackFn();
+                }
+              }}
+            >
+              <Text style={globalStyles.button.text.defaut}>
+                Continue offline
+              </Text>
+            </Pressable>
+          </>
         ) : (
           <></>
         )}
@@ -72,20 +101,10 @@ export default function AuthenticationComponent({
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 10,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   child_view: {
     flex: 1,
-    width: 100,
     height: 100,
     justifyContent: "center",
-    alignItems: "center",
     alignSelf: "center",
-    alignContent: "center",
   },
 });
