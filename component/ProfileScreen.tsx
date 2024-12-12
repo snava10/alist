e80a93939211621bd3d43049fed322561258cf28
snava10 <sergio.nava89@gmail.com>
2024-12-12 09:15:17 +0000

    Aligning profile page

--- component/ProfileScreen.tsx
+++ component/ProfileScreen.tsx
@@ -125,11 +125,18 @@ export default function ProfileScreen({ route }: any) {
         <></>
       )}
       <View
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+        style={
+          isLoggedIn
+            ? {
+                flex: 0.2,
+                flexDirection: "row",
+              }
+            : {
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }
+        }
       >
         <AuthenticationComponent
           isLoggedIn={isLoggedIn}
